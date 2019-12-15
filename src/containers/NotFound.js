a6f7e7c878cd0babf1bdfd0c4b6c50017523d074
Julian Harris <julian@disco-daddy.lan>
2019-12-15 21:35:14 +0000

    404 page

---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NotFound.css';
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h3>Sorry, we don't recognise that web address.</h3>
+    </div>
+  );
+}
