bedc172489514cfbb0a54d132764bd04c46b383d
Julian Harris <julian@disco-daddy.lan>
2019-12-15 21:50:55 +0000

    Amplify v1

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "kiwi-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://tnrr5p75n4.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_lhcHCLxOe",
+    APP_CLIENT_ID: "2lpog6fegkt7e7v0kab4ihfuf9",
+    IDENTITY_POOL_ID: "us-east-1:90a488ce-0bbe-4a2c-9f70-0be22f0c614e"
+  }
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,41 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import config from './config';
+
+// AWS Bridging tech that connects React front-ends 
+// to API Gateway, Storage and Cognito
+// Amplify refers to Cognito as Auth, S3 as Storage, 
+// and API Gateway as API.
+import Amplify from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    // loosen this up later -- we want anonymous users
+    mandatorySignIn: true,
+
+    region:config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        // The name: "notes" is basically telling Amplify that we want to name our API. Amplify allows you to add mul9ple APIs that your app is
+        // going to work with. In our case our en9re backend is just one single API.
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region:config.apiGateway.REGION
+      }
+    ]
+  }
+});
 
 ReactDOM.render(
   <Router>
