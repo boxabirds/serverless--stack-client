1d01aea9a73a8f6f47e9633741102ee885d4cd9a
Julian Harris <julian@disco-daddy.lan>
2019-12-16 22:27:40 +0000

    Loading animation on login

--- src/App.js
+++ src/App.js
@@ -1,66 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { LinkContainer } from "react-router-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import './App.css';
 import Routes from  "./Routes";
 import { Auth } from "aws-amplify";
 
 
-export default function App(props) {
-    const [isAuthenticated, userHasAuthenticated] = useState(false);
-    const [isAuthenticating, setIsAuthenticating] = useState(true);
+function App(props) {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
 
-    useEffect( () => {
-      onLoad();
-    }, []);
+  useEffect( () => {
+    onLoad();
+  }, []);
 
-    async function onLoad() {
-      try {
-        await Auth.currentSession();
-        userHasAuthenticated(true);
-      } catch (e) {
-        if( e !== 'No current user' ) {
-          alert(e);
-        }
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (e) {
+      if( e !== 'No current user' ) {
+        alert(e);
       }
-      setIsAuthenticating(false);
     }
-
-    async function handleLogout() {
-      await Auth.signOut();
-      userHasAuthenticated(false);
-    }
-    
-    return (
-      !isAuthenticating &&
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Notamelo</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-
-              {isAuthenticated
-                ? <NavItem onClick={handleLogout}>Logout</NavItem>
-                : <>
-                  <LinkContainer to="/signup">              
-                    <NavItem>Signup</NavItem> 
-                  </LinkContainer>
-                  <LinkContainer to="/login">              
-                    <NavItem>Login</NavItem>
-                  </LinkContainer>
-                  </>
-              }
-            </Nav>
-          </Navbar.Collapse>
-
-        </Navbar>
-        <Routes appProps = {{ isAuthenticated, userHasAuthenticated }} />
-      </div>
-    )
+    setIsAuthenticating(false);
   }
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    props.history.push("/login");
+  }
+  
+  return (
+    !isAuthenticating &&
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Notamelo</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+
+            {isAuthenticated
+              ? <NavItem onClick={handleLogout}>Logout</NavItem>
+              : <>
+                <LinkContainer to="/signup">              
+                  <NavItem>Signup</NavItem> 
+                </LinkContainer>
+                <LinkContainer to="/login">              
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+                </>
+            }
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
+      <Routes appProps = {{ isAuthenticated, userHasAuthenticated }} />
+    </div>
+  )
+}
+
+export default withRouter(App);
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
 import { Auth } from "aws-amplify";
+import LoaderButton from '../components/LoaderButton';
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -13,10 +15,12 @@ export default function Login(props) {
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
 
     try {
       await Auth.signIn(email, password);
       props.userHasAuthenticated(true);
+      props.history.push("/");
     } catch (e) {
       alert(e.message);
     }
@@ -42,9 +46,15 @@ export default function Login(props) {
             onChange={ e=>setPassword(e.target.value) }
           />
         </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+          </LoaderButton>
       </form>
     </div>
   );
